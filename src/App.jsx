7cf3caa6ce18fa5e7fe73d07b26351a8e3b57c64
Hanasolo <hanasolo@hotmail.com>
2024-11-19 20:38:07 +0100

    restart and useRef rounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from "react"
+import {useState, useEffect, useRef} from "react"
 import Die from "./Die"
 import {nanoid} from "nanoid"
 import Confetti from "react-confetti"
 
+
 export default function App() {
 
-    const [dice, setDice] = React.useState(allNewDice())
-    const [tenzies, setTenzies] = React.useState(false)
+    const [dice, setDice] = useState(allNewDice())
+    const [tenzies, setTenzies] = useState(false)
+    const rounds = useRef(0)
     
-    React.useEffect(() => {
+    
+    useEffect(() => {
         const allHeld = dice.every(die => die.isHeld)
         const firstValue = dice[0].value
         const allSameValue = dice.every(die => die.value === firstValue)
@@ -23,6 +26,7 @@ export default function App() {
             value: Math.ceil(Math.random() * 6),
             isHeld: false,
             id: nanoid()
+            
         }
     }
     
@@ -35,17 +39,22 @@ export default function App() {
         
     }
     
-/**
- * Challenge: Allow the user to play a new game when the
- * button is clicked and they've already won
- */
+
     
     function rollDice() {
-        setDice(oldDice => oldDice.map(die => {
-            return die.isHeld ? 
-                die :
-                generateNewDie()
-        }))
+        if(!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? 
+                    die :
+                    generateNewDie()
+                    
+            }))
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+            rounds.current = 0
+        }
+        
     }
     
     function holdDice(id) {
@@ -55,7 +64,18 @@ export default function App() {
                 die
         }))
     }
+    function counter() {
+         rounds.current =  rounds.current + 1
+         return rounds.current
+         
+          
+    }
     
+    function handleClick() {
+        counter()
+        rollDice()
+    }
+
     const diceElements = dice.map(die => (
         <Die 
             key={die.id} 
@@ -69,17 +89,40 @@ export default function App() {
         <main>
             {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
+            {tenzies ?
+            <>
+            <h1 className="won-title">You won!</h1>
+                <ol className="score-list">Best Score
+                    <li>{rounds.current} Times | 1000 sec.</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ol>
+            </>
+            :
+            <>
             <p className="instructions">Roll until all dice are the same. 
             Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
+            </> 
+            }
+            
             <button 
                 className="roll-dice" 
-                onClick={rollDice}
+                onClick={handleClick}
             >
                 {tenzies ? "New Game" : "Roll"}
             </button>
+            { !tenzies &&
+                   <div className="container-score">
+                     <p className="rounds">Rounds:<br/>{!tenzies && rounds.current}</p>
+                     <p className="timer">Time<br/>1..</p>
+                </div>
+            }
+            
         </main>
     )
 }
